ce7af4b774dfce9524f7ab60d5269db309f8ed77
Andrey Taryverdiev <tariverdiev.andrey77@gmail.com>
2022-07-04 15:33:33 +0300

    test

--- lesson7_array_method/task3/index.js
+++ lesson7_array_method/task3/index.js
@@ -1,4 +1,5 @@
-const sortDesc = numbers => {
-    return numbers.slice().sort((a, b) => b - a)
-}
-sortDesc([1, 2, 3, 4, 5, 6, 7, 8, 9])
+const flatArray = arr => arr.reduce((acc, elem) => {
+    let flat = acc.concat(elem)
+    return flat.slice().sort((a, b) => a - b);
+}, []);
+flatArray([1, 2, 3, [4, 5, 6, ], 7, 8, 9])
