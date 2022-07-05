01168cac76a485021a326a5400d935ab576e2fdc
Andrey Taryverdiev <tariverdiev.andrey77@gmail.com>
2022-07-05 10:42:57 +0300

    test

---
+++ lesson8_objects_basics/task3/index.js
@@ -0,0 +1,26 @@
+const transaction = {
+    currency: 'USD',
+    value: 170,
+    operation: 'sale',
+    agent: {
+        country: 'Ukraine',
+        company: 'NYSE',
+    },
+    'operation time': 1584029990001,
+};
+console.log(transaction.currency)
+console.log(transaction['value'])
+
+/* 1. Выведи в консоль значение свойства 'currency' с помощью точки */
+
+/* 2. Выведи в консоль значение свойства 'value' с помощью квадратных скобок */
+
+const key = 'operation';
+/* 3. Выведи в консоль значение свойства обьекта transaction, название которого находится в переменной key - используй квадратные скобки */
+console.log(transaction[key])
+    /* 4. Введи в консоль значение свойства 'company' используя квадратные скобки */
+console.log(transaction['agent']['company'])
+    /* 5. Выведи в консоль значение свойства 'country' используя точку */
+console.log(transaction.agent.country)
+    /* 6. Выведи в консоль значение свойства 'operation time' используя квадратные скобки */
+console.log(transaction['operation time'])
