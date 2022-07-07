83b3d54a3857d00438bb49c3a85b6694d0e8c16b
Andrey Taryverdiev <tariverdiev.andrey77@gmail.com>
2022-07-07 10:25:46 +0300

    test

---
+++ lesson10_numbers_in_details/task2/index.js
@@ -0,0 +1,8 @@
+const getParsedIntegers = arr => arr
+    .map(el => Number.parseInt(el));
+const getParsedIntegersV2 = arr => arr
+    .map(el => Number.parseInt(el));
+const getParsedFloats = arr => arr
+    .map(el => Number.parseFloat(el));
+const getParsedFloatsV2 = arr => arr
+    .map(el => parseFloat(el));
