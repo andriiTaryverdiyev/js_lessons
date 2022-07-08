c01667ab79d7a99c7f7e17e4f913c72676a667ae
Andrey Taryverdiev <tariverdiev.andrey77@gmail.com>
2022-07-08 16:23:22 +0300

    test

---
+++ lesson12_prctice_with_string/task2/index.js
@@ -0,0 +1,7 @@
+const reverseString = text => {
+    if (typeof text !== 'string') {
+        return null
+    }
+    return text.split("").reverse().join("")
+}
+console.log(reverseString('abcdefg'))
