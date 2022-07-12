0be62b8befbf95d6b015a8a925356ab091dc07da
Andrey Taryverdiev <tariverdiev.andrey77@gmail.com>
2022-07-12 12:35:53 +0300

    test

--- lesson15_closure/task2/index.js
+++ lesson15_closure/task2/index.js
@@ -1,27 +1,27 @@
-export const createCalculator = () => {
-    let mem = 0;
-    return {
-        add: function(addNum) {
-            mem += addNum
-        },
+ export const createCalculator = () => {
+     let mem = 0;
+     return {
+         add: function(addNum) {
+             mem += addNum
+         },
 
-        decrease: function(decNum) {
-            mem -= decNum
-        },
+         decrease: function(decNum) {
+             mem -= decNum
+         },
 
-        reset: function() {
-            mem = 0
-        },
+         reset: function() {
+             mem = 0
+         },
 
-        getMemo: function() {
-            return mem
-        }
+         getMemo: function() {
+             return mem
+         }
 
-    }
+     }
 
-};
-const calc = createCalculator();
-calc.add(10)
-calc.decrease(5)
-console.log(calc.getMemo())
-console.log(calc)
+ };
+ const calc = createCalculator();
+ calc.add(10)
+ calc.decrease(5)
+ console.log(calc.getMemo())
+ console.log(calc)
