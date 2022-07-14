19b4790904960b73e1eba151d404734e1c187efb
Andrey Taryverdiev <tariverdiev.andrey77@gmail.com>
2022-07-14 19:50:28 +0300

    test

--- lesson16_hoisting/task3/index.js
+++ lesson16_hoisting/task3/index.js
@@ -1,3 +1,16 @@
 export function createArrayOfFunctions(num) {
+    if (!num) {
+        return []
+    }
+    if (!Number(num)) {
+        return null
+    }
 
+    let result = []
+    for (let i = 0; i < num; i++) {
+        result.push(() => i)
+    }
+
+
+    return result
 }
