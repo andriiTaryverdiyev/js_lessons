e2c70706d172da4825aec1a9f809435fb05c1f6f
Andrey Taryverdiev <tariverdiev.andrey77@gmail.com>
2022-07-19 10:04:17 +0300

    test

---
+++ lesson19_prototypes/task2/index.js
@@ -0,0 +1,40 @@
+const vehicle = {
+    move() {
+        console.log(`${ship.name} is moving`)
+    },
+    stop() {
+        console.log(`${ship.name} stopped`)
+    }
+};
+const ship = {
+    name: 'Argo',
+    startMachine() {
+        console.log(`${this.name} lifting anchor up`)
+        vehicle.move()
+    },
+    stopMachine() {
+        vehicle.stop()
+        console.log(`${this.name} lifting anchor down`)
+    },
+}
+
+function getOwnProps() {
+    let result = []
+    for (let prop in ship) {
+        if (ship.hasOwnProperty(prop)) {
+            result.push(prop)
+        }
+    }
+    console.log(result)
+}
+getOwnProps()
+
+// export function getOwnProps(obj) {
+//     let result = []
+//     for (let prop in obj) {
+//         if (obj.hasOwnProperty(prop)) {
+//                 result.push(prop)
+//         }
+//     }
+//     return result
+// }
