220095bf0bbd1a00b93f59e2ee3d540b31e4048c
Andrey Taryverdiev <tariverdiev.andrey77@gmail.com>
2022-07-21 10:47:14 +0300

    test

--- lesson20_classes/task4/index.js
+++ lesson20_classes/task4/index.js
@@ -3,6 +3,24 @@ class Order {
         this.price = price
         this.city = city
         this.type = type
+        this.ID = Math.random()
+    }
 
+    checkPrice() {
+        return (this.price > 1000)
+    }
+
+    confirmOrder() {}
+    isValidType() {
+        if (this.type === 'Buy' || this.type === 'Sell') {
+            return true
+        }
+        return false
     }
 }
+
+const order1 = new Order(1100, 'Putzenberg', 'dick')
+
+console.log(order1.checkPrice())
+console.log(order1.confirmOrder())
+console.log(order1.isValidType())
