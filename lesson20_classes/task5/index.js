27f9683eaf81a9698f6b980ae1a00597178a7408
Andrey Taryverdiev <tariverdiev.andrey77@gmail.com>
2022-07-24 20:43:49 +0300

    solution

--- lesson20_classes/task5/index.js
+++ lesson20_classes/task5/index.js
@@ -1,25 +1,53 @@
 class User {
-    id
-    name
-    sessionId
     constructor(id, name, sessionId) {
-        this.id = id
-        this.name = name
-        this.sessionId = sessionId
+        this._id = id
+        this._name = name
+        this._sessionId = sessionId
+
     }
+    get id() {
+        return this._id
+    }
+    get name() {
+        return this._name
+    }
+    get sessionId() {
+        return this._sessionId
+    }
+
 }
 
 class UserRepository {
-    users = []
-    constructor() {
-
+    constructor(users) {
+        this._users = users
+        Object.freeze(users)
+    }
+    get users() {
+        return this._users
+    }
+    getUserNames() {
+        return this._users.map((elem) => elem.name)
+    }
+    getUserIds() {
+        return this._users.map((elem) => elem.id)
+    }
+    getUserNameById(id) {
+        for (let element of this._users) {
+            if (element._id == id) {
+                console.log(element)
+                return element._name
+            }
+        }
     }
 }
 
 // examples
 const user = new User('1', 'Tom', 'session-id');
+const user2 = new User('2', 'Gaspar', 'session-id');
+const userRepository = new UserRepository([user, user2])
 
 // получить свойства можем
 console.log(user.id); // ===> '1'
 console.log(user.name); // ===> 'Tom'
 console.log(user.sessionId); // ===> 'session-id's
+console.log(userRepository.getUserNameById(2))
