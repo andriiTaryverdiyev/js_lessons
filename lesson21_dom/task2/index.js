5afc015d9a00fcefbd4c13830dc255dfa7259df1
Andrey Taryverdiev <tariverdiev.andrey77@gmail.com>
2022-07-25 16:29:22 +0300

    test

--- lesson21_dom/task2/index.js
+++ lesson21_dom/task2/index.js
@@ -1,9 +1,9 @@
-const getTitleElement = () => {
+export const getTitleElement = () => {
     const titleElem = document.querySelector('.title')
     console.dir(titleElem)
     return titleElem
 }
-const getImputElement = () => {
+export const getImputElement = () => {
     const inputElem = document.querySelector('input[type="text"]')
     console.dir(inputElem)
     return inputElem
