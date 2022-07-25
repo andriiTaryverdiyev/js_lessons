05bac9df1a29cb832f9bab4a1448c13d349dc8bd
Andrey Taryverdiev <tariverdiev.andrey77@gmail.com>
2022-07-25 17:34:35 +0300

    test

--- lesson21_dom/task5/index.js
+++ lesson21_dom/task5/index.js
@@ -1,5 +1,5 @@
  export const setTitle = (text) => {
-     const elem = document.querySelector('title').textContent = text
+     const elem = document.querySelector('.title').textContent = text
      return elem
  }
  console.log(setTitle('test'))
