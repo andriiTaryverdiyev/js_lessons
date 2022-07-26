59c7c7425a2ec8708e24e30b4e119a2d2936c54d
Andrey Taryverdiev <tariverdiev.andrey77@gmail.com>
2022-07-26 10:57:53 +0300

    test

--- lesson21_dom/task6/index.js
+++ lesson21_dom/task6/index.js
@@ -1,5 +1,9 @@
 export function setButton(text) {
-    return document
+    return (document
         .querySelector('body')
-        .innerHTML = `<button>  ${text} </button>`
+        .innerHTML = `<button>  ${text} </button>`,
+        document
+        .querySelector('body')
+        .textContent = `<button>  ${text} </button>`
+    )
 }
