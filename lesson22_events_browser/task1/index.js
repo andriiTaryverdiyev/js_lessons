a9e1f5d9b6b2e15573f0632455250ca48e873b60
Andrey Taryverdiev <tariverdiev.andrey77@gmail.com>
2022-07-27 18:15:05 +0300

    test

--- lesson22_events_browser/task1/index.js
+++ lesson22_events_browser/task1/index.js
@@ -1,9 +1,13 @@
+let flag = true
 const divElem = document.querySelector('.rect_div');
 const pElem = document.querySelector('.rect_p');
 const spanElem = document.querySelector('.rect_span');
 
 const clearButton = document.querySelector('.clear-btn')
 const eventsList = document.querySelector('.events-list')
+const removeButton = document.querySelector('.remove-handlers-btn')
+const attachButton = document.querySelector('.attach-handlers-btn')
+
 
 const removeEvents = () => {
     while (eventsList.lastChild) {
@@ -11,7 +15,11 @@ const removeEvents = () => {
     }
 }
 
+
 const logTarget = (text, color) => {
+    if (!flag) {
+        return
+    }
     const eventsListElem = document.querySelector('.events-list');
     eventsListElem.innerHTML += `<span style = "color:${color};margin-left:8px" >${text}</span>`
 };
@@ -33,4 +41,14 @@ divElem.addEventListener('click', logGreyDiv, true)
 pElem.addEventListener('click', logGreyP, true)
 spanElem.addEventListener('click', logGreyspan, true)
 
+const removeHandlers = () => {
+    flag = false
+}
+
+const attachHandlers = () => {
+    flag = true
+}
+
 clearButton.addEventListener('click', removeEvents)
+removeButton.addEventListener('click', removeHandlers)
+attachButton.addEventListener('click', attachHandlers)
