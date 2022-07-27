f676627284e1cb6d2bd81876e05ab02411ac09a0
Andrey Taryverdiev <tariverdiev.andrey77@gmail.com>
2022-07-27 17:42:04 +0300

    test

---
+++ lesson22_events_browser/task1/index.js
@@ -0,0 +1,25 @@
+const divElem = document.querySelector('.rect_div');
+const pElem = document.querySelector('.rect_p');
+const spanElem = document.querySelector('.rect_span');
+
+const logTarget = (text, color) => {
+    const eventsListElem = document.querySelector('.events-list');
+    eventsListElem.innerHTML += `<span style = "color:${color};margin-left:8px" >${text}</span>`
+};
+
+const logGreenDiv = logTarget.bind(null, 'div', 'green')
+const logGreenP = logTarget.bind(null, 'p', 'green')
+const logGreenspan = logTarget.bind(null, 'span', 'green')
+
+const logGreyDiv = logTarget.bind(null, 'div', 'grey')
+const logGreyP = logTarget.bind(null, 'p', 'grey')
+const logGreyspan = logTarget.bind(null, 'span', 'grey')
+
+
+divElem.addEventListener('click', logGreenDiv)
+pElem.addEventListener('click', logGreenP)
+spanElem.addEventListener('click', logGreenspan)
+
+divElem.addEventListener('click', logGreyDiv, true)
+pElem.addEventListener('click', logGreyP, true)
+spanElem.addEventListener('click', logGreyspan, true)
