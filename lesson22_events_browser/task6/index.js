bec5828dc2bb377fd26a1accf4d58956761b441d
Andrey Taryverdiev <tariverdiev.andrey77@gmail.com>
2022-07-27 19:19:30 +0300

    test

--- lesson22_events_browser/task6/index.js
+++ lesson22_events_browser/task6/index.js
@@ -1,5 +1,10 @@
+let flag = true
 const button = document.querySelector('.single-use-btn')
 const consoler = () => {
+    if (!flag) {
+        return
+    }
     console.log('clicked')
+    flag = false
 }
 button.addEventListener('click', consoler)
