68ec6f393155397f488e6396b6ef898d4523ab4d
Andrey Taryverdiev <tariverdiev.andrey77@gmail.com>
2022-07-29 14:53:40 +0300

    test

--- lesson24_working_with_dates/task3/index.js
+++ lesson24_working_with_dates/task3/index.js
@@ -1,24 +1,22 @@
-function dhm(t) {
-    var cd = 24 * 60 * 60 * 1000,
-        ch = 60 * 60 * 1000,
-        d = Math.floor(t / cd),
-        h = Math.floor((t - d * cd) / ch),
-        m = Math.round((t - d * cd - h * ch) / 60000),
-        s = Math.round((t - d * cd - h * ch) / 600000)
-    pad = function(n) { return n < 10 ? '0' + n : n; };
-    if (m === 60) {
-        h++;
-        m = 0;
-    }
-    if (h === 24) {
-        d++;
-        h = 0;
-    }
-    return [d + 'd ', , pad(h) + 'h ', pad(m) + 'm ', pad(s) + 's '].join('');
-}
-
-
 export const getDiff = (startDate, endDate) => {
+    function dhm(t) {
+        var cd = 24 * 60 * 60 * 1000,
+            ch = 60 * 60 * 1000,
+            d = Math.floor(t / cd),
+            h = Math.floor((t - d * cd) / ch),
+            m = Math.round((t - d * cd - h * ch) / 60000),
+            s = Math.round((t - d * cd - h * ch) / 600000)
+        pad = function(n) { return n < 10 ? '0' + n : n; };
+        if (m === 60) {
+            h++;
+            m = 0;
+        }
+        if (h === 24) {
+            d++;
+            h = 0;
+        }
+        return [d + 'd ', , pad(h) + 'h ', pad(m) + 'm ', pad(s) + 's '].join('');
+    }
     let result = 0
     if (startDate >= endDate) {
         result = (startDate - endDate)
